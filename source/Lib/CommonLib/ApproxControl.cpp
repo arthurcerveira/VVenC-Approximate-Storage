1a129378516eb767fba8c1e30410bf953db991f4
Felipe Sampaio <felipe.sampaio@farroupilha.ifrs.edu.br>
2021-07-13 15:52:29 -0300

    Fix m_inputBitDepth attribute at ApproxControl

--- source/Lib/CommonLib/ApproxControl.cpp
+++ source/Lib/CommonLib/ApproxControl.cpp
@@ -3,10 +3,12 @@
 namespace vvenc {
     double ApproxControl::m_deblockingFilterReadBER;
     double ApproxControl::m_deblockingFilterWriteBER;
+    int ApproxControl::m_inputBitDepth;
 
     void ApproxControl::init() {
         m_deblockingFilterReadBER = 0.0;
         m_deblockingFilterWriteBER = 0.0;
+        m_inputBitDepth = 8;
     }
 
 }
